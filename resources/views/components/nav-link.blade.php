5baeaad28199a53da9542c1c9fd06f86d3191abb
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-05-24 14:05:04 +0630

    fix nav link for day mode

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'cursor-pointer px-3 py-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 bg-gray-900 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700'
+            ? 'cursor-pointer px-3 py-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 bg-gray-300 dark:bg-gray-900 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700'
             : 'cursor-pointer px-3 py-2 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700';
 @endphp
 
