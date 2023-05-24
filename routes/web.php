31596b019e553f57b15a1ef425ebe6894b5ad309
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-05-24 10:42:59 +0630

    add email verification

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Auth::routes(['verify' => true]);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', function () {
