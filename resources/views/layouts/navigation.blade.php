c7465a5f38cf3204c7ea4a990aa445f35568b4d4
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-05-24 16:30:50 +0630

    improve header nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,12 +2,27 @@
     <div class="container px-6 py-2 mx-auto">
         <div class="lg:flex lg:items-center lg:justify-between">
             <div class="flex items-center justify-between">
-                <a class="text-white" href="{{ route('home') }}">
+                <a class="text-gray-950 font-bold dark:text-white" href="{{ route('home') }}">
                     {{ config('app.name', 'Demo') }}
                 </a>
 
                 <!-- Mobile menu button -->
                 <div class="flex lg:hidden">
+                    <button class="lg:hidden rounded-md mx-5 focus:outline-none focus:shadow-outline-purple"
+                        @click="toggleTheme" aria-label="Toggle color mode">
+                        <template x-if="!dark">
+                            <svg class="w-5 h-5" aria-hidden="true" fill="gray" viewBox="0 0 20 20">
+                                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                            </svg>
+                        </template>
+                        <template x-if="dark">
+                            <svg class="w-5 h-5" aria-hidden="true" fill="white" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd"
+                                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </template>
+                    </button>
                     <button x-cloak @click="isMobileMenuOpen = !isMobileMenuOpen" type="button"
                         class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
                         aria-label="toggle menu">
@@ -40,8 +55,8 @@
                         </x-nav-link>
                     </div>
                 @endif
-                <button class="rounded-md mx-5 focus:outline-none focus:shadow-outline-purple" @click="toggleTheme"
-                    aria-label="Toggle color mode">
+                <button class="hidden lg:block rounded-md mx-5 focus:outline-none focus:shadow-outline-purple"
+                    @click="toggleTheme" aria-label="Toggle color mode">
                     <template x-if="!dark">
                         <svg class="w-6 h-6" aria-hidden="true" fill="gray" viewBox="0 0 20 20">
                             <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
@@ -81,7 +96,7 @@
                                 </form>
                             </div>
                             <div x-show="isProfileMenuOpen" x-cloak @click.away="isProfileMenuOpen = false"
-                                class="absolute right-0 z-20 w-48 mt-3 overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800 hidden md:block">
+                                class="absolute right-0 z-20 w-48 mt-3 overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800 hidden lg:block">
 
                                 <a href="{{ route('profile.edit') }}"
                                     class="block px-4 py-3 text-sm text-gray-800 transition-colors duration-300 transform border-b dark:text-gray-200 dark:border-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700">
