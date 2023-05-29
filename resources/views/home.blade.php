468be2dce0b6e95974065535e0747b59df32e895
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-05-29 15:25:03 +0630

    add no shoes placeholder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,17 @@
                 :openedTabs="$openedFilterTabs"
             />
         </div>
-        <div>
+        @if($products->count()<=0)
+            <div class="w-full flex content-center justify-center items-center">
+                <span class="bg-gray-300 rounded-md px-20 py-2">No shoes were found.</span>
+            </div>
+        @else
             <div class="py-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <x-products.list :products="$products"/>
                 </div>
             </div>
-        </div>
+        @endif
     </div>
 
 </x-app-layout>
