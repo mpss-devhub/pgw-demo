275ea903f93afe0b96f5e4546f1dc47772b6f58a
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-05-29 15:45:27 +0630

    enable filter in mobileg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 
-    <div class="flex">
-        <div class="w-1/4 py-12 hidden lg:block">
+    <div class="flex flex-col lg:flex-row">
+        <div class="lg:w-1/4 py-4 lg:py-12">
             <x-products.filter
                 :brands="$brands"
                 :categories="$categories"
@@ -15,7 +15,7 @@
                 <span class="bg-gray-300 rounded-md px-20 py-2">No shoes were found.</span>
             </div>
         @else
-            <div class="py-12">
+            <div class="py-4 lg:py-12">
                 <div class="max-w-7xl sm:pl-6 lg:pl-8">
                     <x-products.list :products="$products"/>
                 </div>
