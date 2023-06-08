19b86f33498bd2810aff1320310406b74cf7009d
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-06-08 16:30:25 +0630

    fix qr image component

--- resources/js/components/ShowQRComponent.vue
+++ resources/js/components/ShowQRComponent.vue
@@ -4,7 +4,7 @@
             <div class="p-1 rounded-md">
                <div>
                    <div class="p-8 flex gap-8 flex-col justify-center items-center">
-                       <div class="font-bold text-lg">Scan this QR image from your wallet to finish payment</div>
+                       <div class="font-bold text-lg dark:text-gray-100">Scan this QR image from your wallet to finish payment</div>
                        <img :src="qrImageUrl" width="300" height="300"/>
                    </div>
                </div>
