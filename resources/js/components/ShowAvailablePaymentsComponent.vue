04c63b1bcbc9d750e636055ff9cd702a7cf0c0a7
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-06-12 15:00:28 +0630

    adjust disabled buttons colors to be consistent

--- resources/js/components/ShowAvailablePaymentsComponent.vue
+++ resources/js/components/ShowAvailablePaymentsComponent.vue
@@ -26,7 +26,7 @@
 
         <div class="flex justify-end items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
             <button  @click="onPaymentClicked(null,null)" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
-            <button @click="onContinueClicked" :disabled="!selectedPayment" :class="{'bg-gray-500 dark:bg-gray-600 dark:hover:bg-gray-700 hover:bg-gray-500 cursor-not-allowed':!selectedPayment}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Continue</button>
+            <button @click="onContinueClicked" :disabled="!selectedPayment" :class="{'bg-gray-600 dark:bg-gray-600 dark:hover:bg-gray-600 hover:bg-gray-600 cursor-not-allowed':!selectedPayment}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Continue</button>
         </div>
     </div>
 </template>
--- resources/js/components/ShowPaymentInfoComponent.vue
+++ resources/js/components/ShowPaymentInfoComponent.vue
@@ -46,7 +46,7 @@
 
                         @click="onContinueClicked"
                         :disabled="isContinueButtonDisabled"
-                        :class="{'bg-gray-500  hover:bg-gray-800 cursor-not-allowed':isContinueButtonDisabled,'cursor-pointer':!isContinueButtonDisabled}"
+                        :class="{'bg-gray-600 dark:bg-gray-600  hover:bg-gray-600 dark:hover:bg-gray-600 cursor-not-allowed':isContinueButtonDisabled,'cursor-pointer':!isContinueButtonDisabled}"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     {{isPaymentRequesting?"Processing..":"Continue"}}
                 </button>
